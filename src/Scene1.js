2e7f72070d07c8aad93c59ba7a18949fd3cfc1b6
Adeleke Temitayo <lekedevrepo@gmail.com>
2020-03-13 11:51:18 -0600

    load the sprite in Scene1

--- src/Scene1.js
+++ src/Scene1.js
@@ -8,9 +8,20 @@ export default class Scene1 extends Phaser.Scene {
   preload() {
     // load images
     this.load.image("background", "assets/background.png");
-    this.load.image("ship", "assets/ship.png");
-    this.load.image("ship2", "assets/ship2.png");
-    this.load.image("ship3", "assets/ship3.png");
+    this.load.spritesheet("ship", "assets/sprites/ship.png", {
+      frameWidth: 16,
+      frameHeight: 16
+    });
+
+    this.load.spritesheet("ship2", "assets/sprites/ship2.png", {
+      frameWidth: 32,
+      frameHeight: 16
+    });
+
+    this.load.spritesheet("ship3", "assets/sprites/ship3.png", {
+      frameWidth: 16,
+      frameHeight: 16
+    });
   }
 
   create() {
