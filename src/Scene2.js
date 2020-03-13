af8182a2696504ef810abc22794ef3f16ce0723f
Adeleke Temitayo <lekedevrepo@gmail.com>
2020-03-13 10:54:38 -0600

    add ship images to the game screen

--- src/Scene2.js
+++ src/Scene2.js
@@ -1,5 +1,7 @@
 import "phaser";
 
+import config from "./config";
+
 export default class Scene2 extends Phaser.Scene {
   constructor() {
     super("playGame");
@@ -8,6 +10,19 @@ export default class Scene2 extends Phaser.Scene {
   create() {
     this.background = this.add.image(0, 0, "background");
     this.background.setOrigin(0, 0);
+
+    this.ship1 = this.add.image(
+      config.width / 2 - 50,
+      config.height / 2,
+      "ship"
+    );
+    this.ship2 = this.add.image(config.width / 2, config.height / 2, "ship2");
+    this.ship3 = this.add.image(
+      config.width / 2 + 50,
+      config.height / 2,
+      "ship3"
+    );
+
     this.add.text(20, 20, "Playing game", {
       font: "32px Arial",
       fill: "yellow"
