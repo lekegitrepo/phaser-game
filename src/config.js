9aecb13788c463d079d8cc4197d359d8bafb227b
Adeleke Temitayo <lekedevrepo@gmail.com>
2020-03-13 13:00:21 -0600

    add physics to the game

--- src/config.js
+++ src/config.js
@@ -3,9 +3,15 @@ import "phaser";
 console.log("this config file");
 
 export default {
-	type: Phaser.AUTO,
-	width: 256,
-	height: 272,
-	backgroundColor: 0x000000,
-	pixelArt: true
+  type: Phaser.AUTO,
+  width: 256,
+  height: 272,
+  backgroundColor: 0x000000,
+  pixelArt: true,
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: false
+    }
+  }
 };
