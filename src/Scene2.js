1b0aeef7fc2e31343d1f79182f46adf1e0596232
Adeleke Temitayo <lekedevrepo@gmail.com>
2020-03-13 11:56:02 -0600

    add the sprite to the game scene in Scene2

--- src/Scene2.js
+++ src/Scene2.js
@@ -17,13 +17,13 @@ export default class Scene2 extends Phaser.Scene {
     );
     this.background.setOrigin(0, 0);
 
-    this.ship1 = this.add.image(
+    this.ship1 = this.add.sprite(
       config.width / 2 - 50,
       config.height / 2,
       "ship"
     );
-    this.ship2 = this.add.image(config.width / 2, config.height / 2, "ship2");
-    this.ship3 = this.add.image(
+    this.ship2 = this.add.sprite(config.width / 2, config.height / 2, "ship2");
+    this.ship3 = this.add.sprite(
       config.width / 2 + 50,
       config.height / 2,
       "ship3"
