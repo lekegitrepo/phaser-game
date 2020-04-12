dfb735fcdac4bba4e19af1e2e42549136aa22753
lekegitrepo <lekedevrepo@gmail.com>
2020-04-12 08:40:27 -0500

    preload the font

--- src/Scene1.js
+++ src/Scene1.js
@@ -42,6 +42,8 @@ export default class Scene1 extends Phaser.Scene {
       frameWidth: 16,
       frameHeight: 16
     });
+
+    this.load.bitmapFont("pixelFont", "assets/font/font.png", "assets/font/font.xml");
   }
 
   create() {
